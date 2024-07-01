91c75abd025ec94c0a00e647cf1b1aa1428ad912
dlflqhfl <dlflqhfl@gmail.com>
2024-07-01 17:37:09 +0900

    12112

--- src/main/java/web/main/action/BenefitsAction.java
+++ src/main/java/web/main/action/BenefitsAction.java
@@ -4,7 +4,7 @@ import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
 import java.io.IOException;
 
-public class BenefitsAction implements Action {
+public class git BenefitsAction implements Action {
 
 	@Override
 	public String execute(HttpServletRequest request, HttpServletResponse response) throws IOException {
