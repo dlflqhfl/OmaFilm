a03ec7032873bf227203418095a33e8f7146b984
dlflqhfl <dlflqhfl@gmail.com>
2024-07-01 17:00:38 +0900

    1111

--- src/main/java/web/main/action/BenefitsAction.java
+++ src/main/java/web/main/action/BenefitsAction.java
@@ -9,6 +9,7 @@ public class BenefitsAction implements Action {
 	@Override
 	public String execute(HttpServletRequest request, HttpServletResponse response) throws IOException {
 		// TODO Auto-generated method stub
+		System.out.println("BenefitsAction execute()");
 		return "/jsp/event/benefits.jsp";
 	}
 
